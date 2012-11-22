7ec3c0cb91d8c1a4e44b04735d81a41aed4554fe
morgandan <morgan.dan@gmail.com>
2012-11-22 19:27:02 -0400

    Initial Commit

---
+++ card_share/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardShare::Application.config.secret_token = '3356de775f07f436f870d6f61cf78019100fca990c8470bcd7873d3aaceb0f1166a44dfd5007d684e5d7f22ed238f27a518eb23602277044760e4ab7fd24065f'
